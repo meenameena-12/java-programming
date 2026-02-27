efb35bf11e0f11c9c0620074f594bda3dc2e61d5
meenameena-12 <meenameenameenameena682@gmail.com>
2026-02-27 18:13:26 +0530

    Add files via upload

---
+++ JTextarea.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+public class JTextareaExample{
+  Jframe frame=new Jframe("Textarea example");
+  JTextarea ta=new Textarea(5,20);
+  ta.set bounds(100,70,120,30);
+  frame.add(btn);
+  frame.set size(300,200);
+  frame.setlayout(null);
+  frame.set default close operation(Jframe.exit.on.close);
+  frame.visible("true);
+                  
+                
+                          
+      
+    
