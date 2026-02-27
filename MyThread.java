abf502c87456f1b39390b4812ae8d4336e7a7fdf
meenameena-12 <meenameenameenameena682@gmail.com>
2026-02-27 17:21:21 +0530

    Add files via upload

---
+++ MyThread.java
@@ -0,0 +1,23 @@
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread running: " + i);
+            try {
+                Thread.sleep(1000); 
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.start();
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Main thread: " + i);
+        }
+    }
+}
